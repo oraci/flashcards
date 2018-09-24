229fff96382d02436031b72a7b7ee1e2f223c0f1
Oraci <oraci.izidoro.correa@philips.com>
2018-09-23 23:02:38 -0300

    Flashcards project.

--- App.js
+++ App.js
@@ -1,23 +1,108 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar,
+  View
+} from 'react-native';
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import {Feather, MaterialCommunityIcons} from '@expo/vector-icons';
+import {Constants} from 'expo';
+import DeckMain from './components/DeckMain';
+import DeckDetail from './components/DeckDetail';
+import AddDeck from './components/AddDeck';
+import AddQuestion from './components/AddQuestion';
+import QuizMain from './components/QuizMain';
+import reducer from './reducers'
+import {setLocalNotification} from './utils/helpers';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
   render() {
+    const store = createStore(reducer, {}, applyMiddleware(ReduxThunk));
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <CustomStatusBar
+            backgroundColor="green"
+            barStyle="light-content"
+          />
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+function CustomStatusBar ({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar
+        translucent
+        backgroundColor={backgroundColor}
+        {...props}
+      />
+    </View>
+  );
+}
+
+const TabsNavigator = createBottomTabNavigator({
+  Decks: {
+    screen: DeckMain,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name="home-circle" size={30} color={tintColor} />
+    }
   },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <Feather name="plus-circle" size={30} color={tintColor} />
+    }
+  }
+});
+
+const MainNavigator =  createStackNavigator({
+  Home: {
+    screen: TabsNavigator,
+    navigationOptions: {
+      title: "Flashcards",
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: "green"
+      }
+    }
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: "green"
+      }
+    }
+  },
+  AddQuestion: {
+    screen: AddQuestion,
+    navigationOptions: {
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: "green"
+      }
+    }
+  },
+  QuizMain: {
+    screen: QuizMain,
+    navigationOptions: {
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: "green"
+      }
+    }
+  }
 });
---
+++ components/AddDeck.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {
+  KeyboardAvoidingView,
+  Keyboard
+} from 'react-native';
+import {
+  Button,
+  Card,
+  FormInput,
+  FormValidationMessage
+} from 'react-native-elements';
+import { saveDeckTitle } from '../utils/api';
+
+class AddDeck extends Component {
+  state = {
+    titleText: '',
+    errorMessage: false
+  };
+
+  handleSubmit = () => {
+    if (this.state.titleText) {
+      const { titleText } = this.state;
+      saveDeckTitle(titleText);
+      this.setState({
+        errorMessage: false,
+        titleText: ''
+      });
+      this.props.navigation.navigate(
+        'DeckDetail',
+        {
+          entryId: titleText,
+          navTitle: titleText
+        },
+        Keyboard.dismiss()
+      );
+    } else {
+      this.setState({ errorMessage: true })
+    }
+  };
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={{
+        flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center'
+        }}
+        behavior="padding"
+      >
+        <Card title="What is the title of your new deck?" >
+          <FormInput
+            onChangeText={titleText => this.setState({ titleText })}
+            value={this.state.titleText}
+          />
+          <FormValidationMessage>
+            {this.state.errorMessage ? 'This field is required': ''}
+          </FormValidationMessage>
+          <Button
+            title="Create Deck"
+            raised
+            backgroundColor="rgb(72, 149, 236)"
+            onPress={this.handleSubmit}
+          />
+        </Card>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+export default AddDeck;
